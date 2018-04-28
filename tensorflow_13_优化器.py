b6b55d61351799ede6c82fbd175098eaf00aa2cc
yjfiejd <yjfiejd@163.com>
2018-04-28 18:49:55 +0800

    add_13_Optimizers

---
+++ tensorflow_13_优化器.py
@@ -0,0 +1,16 @@
+# -*- coding:utf8 -*-
+# @TIME : 2018/4/28 下午6:35
+# @Author : Allen
+# @File : tensorflow_13_优化器.py
+
+#Optimaizer 优化器 都是基于learning rate的改变
+#这里需要取补充知识
+
+
+#本次课程，我们会讲到Tensorflow里面的优化器。
+
+#Tensorflow 中的优化器会有很多不同的种类。最基本, 也是最常用的一种就是GradientDescentOptimizer。
+
+#在Google搜索中输入“tensorflow optimizer”可以看到Tensorflow提供了7种优化器：链接
+
+#网址：https://www.tensorflow.org/api_guides/python/train
